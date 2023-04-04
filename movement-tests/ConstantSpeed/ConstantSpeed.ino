7311a3a141a22faa2004f1031c1f8ddaccb44f25
Blake Harkness <blake.ac.harkness@gmail.com>
2023-04-05 11:55:24 +1200

    Added bluetooth functionaility

--- movement-tests/ConstantSpeed/ConstantSpeed.ino
+++ movement-tests/ConstantSpeed/ConstantSpeed.ino
@@ -1,11 +1,15 @@
 #include <AccelStepper.h>
 
 AccelStepper stepper; // Defaults to AccelStepper::FULL4WIRE (4 pins) on 2, 3, 4, 5
-
 bool LAST_BT_COMMAND = 1; //-- motors on be default
+
+char Incoming_value = 0; // Value read from bluetooth
+const int blueLED = 9;
 
 void setup()
 {  
+  Serial.begin(9600);
+  pinMode(blueLED, OUTPUT);
   //-- motors will run by default
    stepper.setMaxSpeed(4000);
    stepper.setSpeed(2000);
@@ -15,11 +19,14 @@ void loop()
 {  
    stepper.runSpeed();
    //-- stop motors
-   if (!LAST_BT_COMMAND) {
+   if (Incoming_value == 3)
+   {
     stepper.setMaxSpeed(0);
     stepper.setSpeed(0);
     LAST_BT_COMMAND = 0;
-   } else if (LAST_BT_COMMAND) {
+   } 
+   else if (Incoming_value == 2) 
+   {
     stepper.setMaxSpeed(4000);
     stepper.setSpeed(2000);
    }
