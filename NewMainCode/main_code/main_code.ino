4f4f0172c347d56761d34782ea4301047dc16b5c
Blake Harkness <blake.ac.harkness@gmail.com>
2023-10-24 17:06:02 +1300

    Added PID Navigation from button push

--- NewMainCode/main_code/main_code.ino
+++ NewMainCode/main_code/main_code.ino
@@ -3,6 +3,10 @@
 #include "Adafruit_SGP40.h"
 #define lidarDebugSerial Serial
 #include "thijs_rplidar.h"
+
+// hardware time include
+#include "driver/timer.h"
+
 // init Class:
 BluetoothSerial ESP_BT;
 Adafruit_SGP40 sgp;
@@ -15,6 +19,97 @@ const int echoPin = 18;
 const int blueLED = 4;
 //MQ Gas Sensor
 const int MQpin = 36;
+
+
+// PID setpoint calcluations
+int setPoint = 0;
+int max_set = 120;
+
+int rm_motor_set = setPoint;
+int lm_motor_set = setPoint;
+
+int lm_motor_change;
+
+// Integral PID component
+float lm_integral = 0;
+float rm_integral = 0;
+
+// Derivative PID component
+float lm_derivative = 0;
+float rm_derivative = 0;
+int lm_prev_error = 0;
+int rm_prev_error = 0;
+
+int lm_error = 0;
+int rm_error = 0;
+
+//int outputRotationSpeed = 0;
+int rm_c = 0;
+int lm_c = 0;
+float L_kp = 0.2; 
+float L_ki = 2.2;
+float L_kd = 0.008; 
+float R_kp = 0.35; 
+float R_ki = 3.0;
+float R_kd = 0.05;
+
+int store_counts, store_counts1;
+int lm_set_flag_100ms;
+int rm_set_flag_100ms;
+
+// Lidar stuff -----------------------------
+int distFloat = 0;
+float angleDegreesFloat = 0;
+//float *dist_p = &distFloat;
+//float *angle_p = &angleDegreesFloat;
+
+int distTemp = 0;
+float angleTemp = 0;
+
+int *dist_p = &distFloat;
+float *angle_p = &angleDegreesFloat;
+float prev_dist = 0;
+// -----------------------------------------
+
+//-- Stopping using PID
+int wait_360 = 0;   //-- checks around before move forward
+int turn_count = 0;
+
+int drive_or_turn = 1;  // 0 nothing, 1 drive, 2 turn
+
+//  Turning
+int lm_direction = 1;   // 1 forward, -1 reverse
+int rm_direction = 1;   // 1 forward, -1 reverse
+
+int angle_count_lm = 0;
+int angle_count_rm = 0;
+
+#define lidarDebugSerial Serial // Lidar serial
+
+// left and right motor enables
+#define motor_enable 4
+
+// motor directions pins, send PWM singal to direction
+#define LM_F 19 // left motor _ foward
+#define LM_R 18 // left motor _ reverse
+
+#define RM_F 33  //2
+#define RM_R 32 //15 change to these, or match pcb document
+
+// encoder definitions
+
+#define LM_ENC_1 2 //5 - this can all be found on the pcb document!
+#define LM_ENC_2 15 //17
+
+#define RM_ENC_1 26 //16 change to this
+#define RM_ENC_2 25  //4 
+
+// encoder count
+long rm_counts = 0;
+long lm_counts = 0;
+long counts_total = 0;
+// timer instantiation for hardware sampling overflow, 100ms (I believe)
+hw_timer_t *Timer0_Cfg = NULL;
 
 
 //Variables and constants
@@ -49,6 +144,152 @@ void devPageBluetooth(void);
 void checkPageUpdate(void);
 void sendLidarBT(float, float);
 void polarToCart(float dst, float ang);
+
+
+void readEncoder() //this function is triggered by the encoder CHANGE, and increments the encoder counter
+
+{ 
+  lm_counts++; //left motor encoder count increment
+  if(drive_or_turn == 2)
+  {
+    angle_count_lm++;
+  }
+}
+
+void readEncoder1() //this function is triggered by the encoder CHANGE, and increments the encoder counter
+
+{ 
+  rm_counts++; //right motor encoder count increment
+  if(drive_or_turn == 2)
+  {
+    angle_count_rm++;
+  }
+}
+
+// sampling isr which happens every 100ms
+void IRAM_ATTR Timer0_ISR() //-- happens every 100ms
+{
+
+    lm_prev_error = lm_c;
+    rm_prev_error = lm_c;
+    
+    // read and update wheel linear velocity
+    rm_c = rm_counts;
+    lm_c = lm_counts;
+
+    if(setPoint < max_set && drive_or_turn != 0) setPoint += 5;
+    
+
+    rm_counts = 0; //-- reset counts
+    lm_counts = 0;
+    
+    lm_set_flag_100ms = 1;
+    rm_set_flag_100ms = 1;
+
+}
+
+void init_motors() {
+  // set motors to output mode
+  pinMode(motor_enable, OUTPUT);
+  
+  pinMode(LM_F, OUTPUT);
+  pinMode(LM_R, OUTPUT);
+  pinMode(RM_F, OUTPUT);
+  pinMode(RM_R, OUTPUT);
+}
+
+void enable_motors() {  
+  digitalWrite(motor_enable, HIGH);  // enable motors
+}
+
+void disable_motors() {
+  digitalWrite(motor_enable, LOW);  // enable motors
+}
+
+void left_speed(int speed_, int dir_) {
+  if (dir_ == 1) {
+    analogWrite(LM_F, speed_);
+    digitalWrite(LM_R, 0);
+    digitalWrite(LM_F, 1);
+  }
+  if (dir_ == -1) {
+    analogWrite(LM_R, speed_);
+    digitalWrite(LM_F, 0);
+    digitalWrite(LM_R, 1);
+  }
+}
+
+// right_speed controller set pwm speed 0 - 255 (2^8)
+void right_speed(int speed_, int dir_) {
+  if (dir_ == 1) {
+    analogWrite(RM_F, speed_);
+    digitalWrite(RM_R, 0);
+    digitalWrite(RM_F, 1);
+  }
+  if (dir_ == -1) {
+    analogWrite(RM_R, speed_);
+    digitalWrite(RM_F, 0);
+    digitalWrite(RM_R, 1);
+  }
+}
+
+void PID_control_lm(int m_dir)
+{
+  if (lm_set_flag_100ms == 1)
+  {
+    lm_error = setPoint-lm_c;
+    
+    lm_integral += 0.1*lm_error;  // Integral component
+    
+    lm_derivative = (lm_error-lm_prev_error)/0.1;
+
+    lm_motor_set = L_kp*lm_error + L_ki*lm_integral + L_kd*lm_derivative;   // Changing left motors set speed
+
+    if (lm_motor_set <=0 || setPoint == 0)
+    {
+      lm_motor_set = 0;
+      lm_integral = 0;
+      lm_error = 0;
+      lm_motor_set = 0;
+    }
+    
+    else if (lm_motor_set > 255) lm_motor_set = 255;
+
+   
+    
+    left_speed(lm_motor_set, m_dir); 
+    
+    lm_set_flag_100ms = 0;
+  }  
+}
+
+void PID_control_rm(int m_dir)
+{
+  if (rm_set_flag_100ms == 1)
+  {
+    rm_error = setPoint-rm_c;
+    rm_integral += 0.1*rm_error;  // Integral component
+    
+    rm_derivative = (rm_error-rm_prev_error)/0.1;
+
+    rm_motor_set = R_kp*rm_error + R_ki*rm_integral + R_kd*rm_derivative;   // Changing left motors set speed
+
+    if (rm_motor_set <=0 || setPoint == 0)
+    {
+      rm_motor_set = 0;
+      rm_integral = 0;
+      rm_error = 0;
+      rm_derivative = 0;
+    }
+    
+    else if (rm_motor_set > 255) rm_motor_set = 255;
+    
+    
+    right_speed(rm_motor_set, m_dir); 
+    
+    rm_set_flag_100ms = 0;
+  }  
+}
 
 // LIDAR STUFF (THIJSES EXAMPLE)--------------------------------------------------------------------------
 struct lidarMotorHandler {  // not really needed (and (currently) very ESP32-bound) but somewhat futureproof
@@ -146,6 +387,20 @@ void setup()
   Serial.print(sgp.serialnumber[1], HEX);
   Serial.println(sgp.serialnumber[2], HEX);
 
+  pinMode(LM_ENC_1, INPUT); //initialize Encoder Pins
+  pinMode(LM_ENC_2, INPUT);
+  
+  digitalWrite(LM_ENC_1, LOW); //initialize Pin States
+  digitalWrite(LM_ENC_2, LOW);
+  
+  attachInterrupt(digitalPinToInterrupt(LM_ENC_1), readEncoder, CHANGE); //attach interrupt to PIN 2 
+  attachInterrupt(digitalPinToInterrupt(RM_ENC_1), readEncoder1, CHANGE); //attach interrupt to PIN 2
+  
+  Timer0_Cfg = timerBegin(0, 80, true);
+  timerAttachInterrupt(Timer0_Cfg, &Timer0_ISR, true);
+  timerAlarmWrite(Timer0_Cfg, 100000, true);
+  timerAlarmEnable(Timer0_Cfg);
+
   //LIDAR STUFF------------------------------------------------------------------------------------------------------
     motorHandler.init();
 
@@ -171,11 +426,68 @@ void setup()
   bool startSuccess = lidar.startExpressScan(EXPRESS_SCAN_WORKING_MODE_BOOST);
 //  Serial.print("startSuccess: "); Serial.println(startSuccess);
 //------------------------------------------------------------------------------------------------------------------
+
+  init_motors();
+  enable_motors();
+  delay(1000); 
 }
 
 void loop() 
 {
   bluetoothMode();
+}
+
+
+void stop_motors()
+{
+  //disable_motors();
+  drive_or_turn = 0;
+  setPoint = 0; // Disable motors 
+  
+  left_speed(0,1);
+  right_speed(0,1);
+
+  //Serial.println("motors stopped");
+}
+
+void start_motors()
+{
+  enable_motors();
+  //drive_or_turn = 1;  
+
+ // Serial.println("start motors");
+}
+
+void turn_to_angle(int angle)
+{
+  // clear encoder angle counters
+  if(angle_count_rm >= 16.9*angle)
+  {
+    right_speed(0, -1);
+    
+    stop_motors();
+
+    drive_or_turn = 1;
+
+    turn_count = 0;
+  }
+}
+
+void stop_then_turn()
+{
+  if ((*angle_p > 39 && *angle_p < 141) && *dist_p <= 230 && *dist_p >= 180 && turn_count == 0) // If threshold met in relevent cone
+  {
+    stop_motors();
+    turn_count = 1;
+
+    angle_count_lm = 0;
+    angle_count_rm = 0;
+
+    drive_or_turn = 2;
+
+    left_speed(100, 1);
+    right_speed(100, -1);
+  }
 }
 
 void connectPageBluetooth() //Whats happening on the connect page
@@ -194,8 +506,41 @@ void connectPageBluetooth() //Whats happening on the connect page
     else if(Incoming_value == 5) //Start cleaning has been pressed
     {
       Serial.println("cleaning starting");
-        //call function to start cleaning
+      while(1)
+      {
+          // Lidar stuff -----------------------------
+        if(keepSpinning) {
+        uint32_t extraSpeedTimer = micros();
+        int8_t datapointsProcessed = lidar.handleData(false, false); // read lidar data and send it to the callback function. Parameters are: (includeInvalidMeasurements, waitForChecksum)
+        // includeInvalidMeasurements means sending data where the measurement failed (out of range or too close or bad surface, etc. it's when distance == 0)
+        // waitForChecksum (only applies to express scans) means whether you wait for the whole packet to come, or to process data as it comes in (checksum is still checked when the whole packet is there, but the bad data may have already been sent to the callback)
+        //    extraSpeedTimer = micros() - extraSpeedTimer;
+        //    if(extraSpeedTimer > 40) { Serial.println(extraSpeedTimer); }
+    
+        if(datapointsProcessed < 0) { keepSpinning = false; lidar.stopScan(); } // handleData() returns -1 if it encounters an error
+        //if(lidar.packetCount >= 200) { keepSpinning = false; lidar.stopScan(); }  // stop scanning after a while
+    
+      } 
+    else 
+    {
+      motorHandler.setPWM(0);
     }
+    // -----------------------------------------
+    stop_then_turn();
+    if (turn_count == 1)
+    {
+      turn_to_angle(180);  
+    }
+    if(drive_or_turn == 1 || drive_or_turn == 0)
+    {
+      PID_control_lm(lm_direction);
+      PID_control_rm(rm_direction);
+    }
+
+
+    }
+    }
+     
     else if(Incoming_value == 6) //Stop cleaning has been pressed
     {
       
@@ -225,12 +570,12 @@ void mappingPageBluetooth()
   bool carryOn = true;
   while(carryOn) // change to not equal to different page value
   {
-    Incoming_value = ESP_BT.read(); //Read what we receive 
-    if(Incoming_value == 4 || Incoming_value == 3 || Incoming_value == 2)
-    {
-      pageOpen = Incoming_value;
-      carryOn = false;
-    } 
+  //  Incoming_value = ESP_BT.read(); //Read what we receive 
+   // if(Incoming_value == 4 || Incoming_value == 3 || Incoming_value == 2)
+  //  {
+   //   pageOpen = Incoming_value;
+   //   carryOn = false;
+   // } 
 
     // LIDAR STUFF ---------------------------------------------------------------------------------------------------
     //  if(Serial.available()) { lidar.lidarSerial.write(Serial.read()); }
@@ -382,3 +727,24 @@ void polarToCart(float dst, float ang) // Convert polar coordinates to rect/cart
 //  Serial.println();
 
 }
+
+
+class transverse_space
+{
+  int grid_step_over = 350; //-- diameter of the roomba
+
+  bool visable_boundary[36][36] = {};
+
+  int raw_linear_data[360][2] = {};
+
+  void establish_graph_boundaries(int in[360][2]) {
+    
+  }
+
+  //-- as the roomba navigates around the 0 position will change. this means that data fed into the array will be all wrong.    
+  
+  void find_best_path() {
+    
+  }
+    
+};
